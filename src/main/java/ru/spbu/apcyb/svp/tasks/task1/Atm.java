36503ff29ea3c8d1c535866f827c73fcfaafdf17
AnSaladim <sharepin.assassin@gmail.com>
2023-12-04 14:32:26 +0300

    new start

---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task1/Atm.java
@@ -0,0 +1,68 @@
+package ru.spbu.apcyb.svp.tasks.task1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class Atm {
+
+    /**
+     * Алгоритм для нахождения комбинаций.
+     *
+     * @param amount - сумма для размена.
+     * @param numbers - номиналы для размена.
+     * @param currentCombination - текущая комбинации.
+     * @param combinations - всевозможные комбинации.
+     */
+    public static void calcCombinations(long amount, List<Long> numbers,
+                                        List<Long> currentCombination,
+                                        List<List<Long>> combinations, int i) {
+
+        if (amount == 0) {
+            combinations.add(new ArrayList<>(currentCombination));
+            return;
+        }
+
+        while (i < numbers.size() && amount - numbers.get(i) >= 0) {
+            currentCombination.add(numbers.get(i));
+            calcCombinations(amount - numbers.get(i), numbers, currentCombination, combinations, i);
+            i++;
+            currentCombination.remove(currentCombination.size() - 1);
+        }
+    }
+
+    /**
+     * Метод, который вычисляет всевозможные варианты разложения суммы по имеющимся номиналам.
+     *
+     * @param sum - сумма, которую нужно разменять.
+     * @param numbers - имеющиеся номиналы.
+     *
+     * @return список разложений в сумму по номиналам. Список будет пуст, если sum
+     *     невозможно разложить по данным номиналам.
+     * @throws IllegalArgumentException - если среди суммы и номиналов есть
+     *     неположительные.
+     * @throws InputMismatchException - номиналов есть null.
+     */
+    public static List<List<Long>> getCombinations(long sum, List<Long> numbers) {
+        if (numbers == null) {
+            throw new InputMismatchException("Numbers must not be null");
+        }
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException("Numbers must not be empty");
+        }
+        if (sum <= 0 || numbers.stream().anyMatch(n -> (n <= 0))) {
+            throw new IllegalArgumentException("Sum and numbers must be positive");
+        }
+
+        numbers = new ArrayList<>(numbers.stream().distinct().toList());
+        Collections.sort(numbers);
+
+        List<Long> currentCombination = new ArrayList<>();
+        List<List<Long>> combinations = new ArrayList<>();
+
+        calcCombinations(sum, numbers, currentCombination, combinations, 0);
+        return combinations;
+    }
+
+}
---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task1/Task1.java
@@ -0,0 +1,53 @@
+package ru.spbu.apcyb.svp.tasks.task1;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
+
+import static java.lang.Long.parseLong;
+import static ru.spbu.apcyb.svp.tasks.task1.Atm.getCombinations;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter amount: ");
+        long sum = parseLongg(sc.nextLine());
+
+        System.out.print("Enter denominations: ");
+        String numbers = sc.nextLine();
+        List<Long> nums = new ArrayList<>(Arrays
+                .stream(numbers.split("\\s"))
+                .filter(s -> !s.isEmpty())
+                .map(Task1::parseLongg)
+                .toList());
+        sc.close();
+
+        List<List<Long>> ans = getCombinations(sum, nums);
+        System.out.print("Сombination count: ");
+        System.out.println(ans.size());
+        System.out.println("All combinations: ");
+        ans.forEach(System.out::println);
+    }
+
+    public static long parseLongg(String s) {
+        try {
+            return parseLong(s);
+        } catch (NumberFormatException e) {
+            String forStr = "For string \"" + s + "\"";
+            if (Pattern.compile("[^-+0-9]").matcher(s).find()) {
+                throw new NumberFormatException(
+                        forStr + ": an integer consists only of a sign and digits from 0 to 9");
+            }
+            int pc= StringUtils.countMatches(s, '+');
+            int mc = StringUtils.countMatches(s, '-');
+            if (pc + mc > 1){
+                throw new NumberFormatException(
+                        forStr + ": too many sign symbols for an integer");
+            }
+            throw new NumberFormatException(
+                    forStr + ": the sign must go ahead of a number");
+        }
+    }
+}
