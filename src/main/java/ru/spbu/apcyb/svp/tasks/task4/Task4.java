4756bbb11181d16b7f0629d337811129f1d850e4
AnSaladim <sharepin.assassin@gmail.com>
2023-12-09 23:30:07 +0300

    fix sonarCloud

--- src/main/java/ru/spbu/apcyb/svp/tasks/task4/Task4.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/task4/Task4.java
@@ -20,60 +20,67 @@ class Task4 {
         multithreadedTan("num.txt", "num_tan.txt", size, 8);
     }
     public static void generateNumbersFile(String fileWriteName, int size) throws IOException {
-        FileWriter fileWriter = new FileWriter(fileWriteName, false);
-        for (int i = 0; i < size; i++) {
-            fileWriter.write(Float.toString(ThreadLocalRandom.current().nextFloat(-1000, 1000)) + " ");
+        try (FileWriter fileWriter = new FileWriter(fileWriteName, false)) {
+            for (int i = 0; i < size; i++) {
+                fileWriter.write(Float.toString(ThreadLocalRandom.current().nextFloat(-1000, 1000)) + " ");
+            }
+            fileWriter.flush();
         }
-        fileWriter.flush();
     }
 
     public static boolean oneThreadTan(String fileReadName, String fileWriteName, int size) throws IOException {
-        FileReader fileReader = new FileReader(fileReadName);
-        FileWriter fileWriter = new FileWriter(fileWriteName, false);
-        fileWriter.write("Количество вычисленных значений: " + size + "\n");
+        long start;
+        try (FileReader fileReader = new FileReader(fileReadName)) {
+            try (FileWriter fileWriter = new FileWriter(fileWriteName, false)) {
+                fileWriter.write("Количество вычисленных значений: " + size + "\n");
 
-        BufferedReader fileBReader = new BufferedReader(fileReader);
-        StringBuilder sb = new StringBuilder("").append(fileBReader.readLine());
-        String[] tmp = sb.toString().split(" ");
+                BufferedReader fileBReader = new BufferedReader(fileReader);
+                StringBuilder sb = new StringBuilder("").append(fileBReader.readLine());
+                String[] tmp = sb.toString().split(" ");
 
-        long start = System.nanoTime();
-        for (String integer : tmp) {
-            final double i = parseDouble(integer);
-            fileWriter.write(Math.tan(i) + " ");
+                start = System.nanoTime();
+                for (String integer : tmp) {
+                    final double i = parseDouble(integer);
+                    fileWriter.write(Math.tan(i) + " ");
+                }
+                fileWriter.flush();
+            }
         }
-        fileReader.close();
-        fileWriter.flush();
         System.out.println(format("One thread Executed by %d ns, size : %d",
                 (System.nanoTime() - start), size));
         return true;
     }
 
     public static boolean multithreadedTan(String fileReadName, String fileWriteName, int size, int nThreads) throws IOException, ExecutionException, InterruptedException {
-        FileReader fileReader = new FileReader(fileReadName);
-        FileWriter fileWriter = new FileWriter(fileWriteName, false);
-        fileWriter.write("Количество вычисленных значений: " + size + "\n");
+        try (FileReader fileReader = new FileReader(fileReadName)) {
+            try (FileWriter fileWriter = new FileWriter(fileWriteName, false)) {
+                fileWriter.write("Количество вычисленных значений: " + size + "\n");
 
-        BufferedReader fileBReader = new BufferedReader(fileReader);
-        StringBuilder sb = new StringBuilder("").append(fileBReader.readLine());
-        String[] tmp = sb.toString().split(" ");
+                StringBuilder sb;
+                try (BufferedReader fileBReader = new BufferedReader(fileReader)) {
+                    sb = new StringBuilder("").append(fileBReader.readLine());
+                }
+                String[] tmp = sb.toString().split(" ");
 
-        ExecutorService threadPool = Executors.newFixedThreadPool(nThreads);
-        long start = System.nanoTime();
+                ExecutorService threadPool = Executors.newFixedThreadPool(nThreads);
+                    long start = System.nanoTime();
 
-        List<CompletableFuture<Double>> futures = new ArrayList<>();
-        for (String integer : tmp) {
-            final double i = parseDouble(integer);
-            futures.add(CompletableFuture.supplyAsync(() -> Math.tan(i), threadPool));
+                    List<CompletableFuture<Double>> futures = new ArrayList<>();
+                    for (String integer : tmp) {
+                        final double i = parseDouble(integer);
+                        futures.add(CompletableFuture.supplyAsync(() -> Math.tan(i), threadPool));
+                    }
+                    CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
+                    for (Future<Double> future : futures) {
+                        fileWriter.write(String.valueOf(future.get()) + " ");
+                    }
+                    fileWriter.flush();
+                    System.out.println(format("Multi thread Executed by %d ns, size : %d",
+                            (System.nanoTime() - start), size));
+                    threadPool.shutdown();
+
+            }
         }
-        fileReader.close();
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
-        for (Future<Double> future : futures) {
-            fileWriter.write(String.valueOf(future.get()) + " ");
-        }
-        fileWriter.flush();
-        System.out.println(format("Multi thread Executed by %d ns, size : %d",
-                (System.nanoTime() - start), size));
-        threadPool.shutdown();
         return true;
     }
 
